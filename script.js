2d264d94e8b56808d7ee2ee9ad5bb8bb5a18a112
asgaard777 <asgaard777@gmail.com>
2025-11-14 10:40:34 -0300

    Initial commit

--- script.js
+++ script.js
@@ -1,111 +1,50 @@
 // Vanilla-JS version for the slide-animation code
 
 window.addEventListener('load', function () {
-    const previousBtn = document.querySelector('#previous');
-    const nextBtn = document.querySelector('#next');
-    const slides = document.querySelector("#slider ul");
-
+    const slideWindow = document.querySelector("#slider");
+    const slide = document.querySelector("#slider ul");
     const SLIDE_WIDTH = document.querySelector('img').offsetWidth;
-    const NUMBER_OF_SLIDES = document.querySelector('#slider > ul').childElementCount;
-    let countRight = NUMBER_OF_SLIDES - 1;
-
-    previousBtn.disabled = true;
-    previousBtn.style.opacity = 0.4;
+    const imgList = document.querySelectorAll('#slider ul li');
 
 
-    let currentOffset = 0;
-    function move(distance) {
-        const from = currentOffset;
-        const to = currentOffset + distance;
-        slides.animate(
-            [
-                { transform: `translateX(${from}px)` },
-                { transform: `translateX(${to}px)` }
-            ],
-            {
-                duration: 800,
-                fill: "forwards",
-                easing: "ease-out"
-            }
-        );
-        currentOffset = to;
+    class Slider {
+        constructor(slideElement, currentOffset = 0) {
+            this.slide = slideElement;
+            this.currentOffset = currentOffset;
+        }
+
+        move(distance) {
+            const from = this.currentOffset;
+            const to = this.currentOffset + distance;
+
+            this.slide.animate(
+                [
+                    { transform: `translateX(${from}px)` },
+                    { transform: `translateX(${to}px)` }
+                ],
+                {
+                    duration: 600,
+                    fill: "forwards",
+                    easing: "ease-out"
+                }
+            );
+            this.currentOffset = to;
+        }
     }
 
-    function setButtonStatus(btn, isDisabled, opacity) {
-        btn.disabled = isDisabled;
-        btn.style.opacity = opacity;
+    let mySlider = new Slider(slide);               
+    let firstChild = slide.firstElementChild;
+
+    function moveSlide() {
+        mySlider.move(-SLIDE_WIDTH);     
+        firstChild.remove();
+        slide.appendChild(firstChild);
+        firstChild = slide.firstElementChild;         
+        mySlider = new Slider(slide);
     }
 
-    nextBtn.addEventListener('click', function () {
-        if (countRight >= 1 && countRight <= NUMBER_OF_SLIDES - 1) {
-            --countRight;
-            move(-SLIDE_WIDTH);
-            setButtonStatus(previousBtn, false, 1);
-
-            if (countRight == 0) setButtonStatus(nextBtn, true, 0.4);
-        }
-    });
-
-
-    previousBtn.addEventListener('click', function () {
-        if (countRight >= 0 && countRight <= NUMBER_OF_SLIDES - 2) {
-            countRight++;
-            move(SLIDE_WIDTH);
-            setButtonStatus(nextBtn, false, 1);
-
-            if (countRight == NUMBER_OF_SLIDES - 1) setButtonStatus(previousBtn, true, 0.4);
-        }
-    });
-})  
-
-
-
-
-
-// JQuery version for the slide-animation code
-
-/* $(window).on('load', function () {
-    const $previousBtn = $('#previous');
-    const $nextBtn = $('#next');
-    const $slides = $("#slider ul");
-
-    const $SLIDE_WIDTH = $('img').width();
-    const $NUMBER_OF_SLIDES = $('#slider > ul').children().length;
-    let $countRight = $NUMBER_OF_SLIDES - 1;
-
-    $previousBtn.prop('disabled', true);
-    $previousBtn.css('opacity', 0.4);
-
-    function setButtonStatus($btn, $isDisabled, $opacity) {
-        $btn.prop('disabled', $isDisabled);
-        $btn.css('opacity', $opacity);
-    }
-
-    $nextBtn.click(function () {
-        if ($countRight >= 1 && $countRight <= $NUMBER_OF_SLIDES - 1) {
-            --$countRight;
-            let $moveLeft = '-=' + $SLIDE_WIDTH;
-            $slides.animate({ left: $moveLeft }, 500);
-            setButtonStatus($previousBtn, false, 1);
-
-            if ($countRight == 0) setButtonStatus($nextBtn, true, 0.4);
-        }
-    });
-
-    $previousBtn.click(function () {
-        if ($countRight >= 0 && $countRight <= $NUMBER_OF_SLIDES - 2) {
-            $countRight++;
-            let $moveRight = '+=' + $SLIDE_WIDTH;
-            $slides.animate({ left: $moveRight }, 500);
-            setButtonStatus($nextBtn, false, 1);
-
-            if ($countRight == $NUMBER_OF_SLIDES - 1) setButtonStatus($previousBtn, true, 0.4);
-        }
-    });
+    let interval = setInterval(moveSlide, 900);    
 })
- */
-
-
 
 
 
